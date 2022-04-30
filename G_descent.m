5026bbbbdf33d3e9de5ffada71ffb12cd27e8546
Pietrosanguin <75418087+Pietrosanguin@users.noreply.github.com>
2022-04-30 14:18:48 +0200

    Second commit
    Ho creato una funzione sulla falsariga di quella del prof, però la norma del gradiente aumenta al posto di diminuire...

---
+++ G_descent.m
@@ -0,0 +1,174 @@
+ function [x,it,fx,ttot,fh,timeVec,gnrit] = G_descent(w_ij,y_lab,w_bar_ij,y_un,lc,verbosity,arls,maxit,eps,fstop,stopcr)
+
+% Parametri di output della funzione: 
+%x:  è il punto di minimo a cui vogliamo convergere
+%it:  è il l'iterazione a cui siamo arrivati
+%fx:  è il valore della funzione aggiornato ad ogni iterazione
+%ttot: contiene il cpu time totale 
+%fh: è un array che contiene il valore della funzione all'iterazione it
+%timeVec: è un array che contiene il cpu time ad ogni iterazione
+%gnrit: è la norma del gradiente ad ogni iterazione it
+%
+% Parametri di input
+%y_un che do' in input è lo starting point
+%w_ij, w_bar_ij, y:un sono parametri del problema
+%lc: è la costante di Lipschitz (poi vedremo come determinarla)
+%arls: è il tipo di Armijo line search che può essere 1 2 o 3 per il momento ho guardato solo 1 e 3 
+%verbosity: è un parametro che se >0 mostra cosa fa l'algoritmo ad ogni iterazione
+
+global lc;
+        
+gamma=0.0001;
+maxniter=maxit;
+fh=zeros(1,maxit);
+gnrit=zeros(1,maxit);
+timeVec=zeros(1,maxit);
+flagls=0;
+
+tic; %fa partire il calcolo del tempo
+timeVec(1) = 0;
+
+%Values for the smart computation of the o.f. 
+%Calcolo le due parti della funzione da minimizzare e poi le sommo tra loro
+%sum1 e sum2 sono la somma di tre termini che escono dallo sviluppo dei
+%quadrati
+
+sum1 = sum(w_ij*(y_un.^2))+(y_lab.^2).'*sum(w_ij,2)-2*y_lab.'*(w_ij*y_un);
+sum2 = sum(w_bar_ij*(y_un.^2))+(y_un.^2).'*sum(w_bar_ij,2)-2*y_un.'*(w_bar_ij*y_un);
+
+fx=sum1 + 0.5*sum2;   %nella notazione del prof è fx nel nostro caso sarebbe fy
+
+
+it=1;
+
+ for j= 1:length(y_un)
+    g = transpose(2*(y_un(j)-y_lab).'*w_ij+2*(y_un(j)-y_un).'*w_bar_ij);
+    
+ end 
+
+while (flagls==0)
+    %vectors updating
+    if (it==1)
+        timeVec(it) = 0;
+    else
+        timeVec(it) = toc;
+    end
+    fh(it)=fx;
+    
+    % gradient evaluation, ho sostituito il nostro gradiente con quello
+    % calcolato da lui nel vecchio script
+     
+    d=-g;
+    
+    %gnr è la norma del gradiente, viene salvata nel vettore gnrit per ogni
+    %iterazione it
+    gnr = g'*d;
+    gnrit(it) = -gnr;
+        
+        % stopping criteria and test for termination
+    if (it>=maxniter)
+        break;
+    end
+        switch stopcr  
+            case 1
+                % continue if not yet reached target value fstop
+                if (fx<=fstop)
+                    break
+                end
+            case 2
+                % stopping criterion based on the product of the 
+                % gradient with the direction
+                if (abs(gnr) <= eps)
+                    break;
+                end
+            otherwise
+                error('Unknown stopping criterion');
+        end % end of the stopping criteria switch
+        
+        
+        
+        %linesearch
+        %arls=2;
+        
+        switch arls
+            case 1
+                 %Armijo search
+            alpha=3;
+            ref = gamma*gnr;
+            
+            while(1)
+                z=y_un+alpha*d;
+                %Computation of the o.f. at the trial point
+                sum1z = sum(w_ij*(z.^2))+(y_lab.^2).'*sum(w_ij,2)-2*y_lab.'*(w_ij*z);
+                sum2z = sum(w_bar_ij*(z.^2))+(z.^2).'*sum(w_bar_ij,2)-2*z.'*(w_bar_ij*z);
+
+                fz=sum1z + 0.5*sum2z;
+                         
+                if (fz<=fx+alpha*ref)
+                    z = y_un + alpha*d;
+                    break;
+                else
+                    alpha=alpha*0.1;
+                end
+                
+                if (alpha <= 1e-20)
+                    z=y_un;
+                    fz=fx;
+                    flagls=1;
+                    it = it-1;
+                    break;
+                end
+                
+            end
+            case 2
+                %exact alpha
+                alpha=-gnr/((d'*Q)*d);
+                z=x+alpha*d;
+                Qz = Q*z;
+                zQz= z'*Qz;
+                cz = c'*z;
+                fz = 0.5*zQz-cz;
+            otherwise
+               %fixed alpha
+                alpha=1/lc;
+                z=y_un+alpha*d;
+                sum1z = sum(w_ij*(z.^2))+(y_lab.^2).'*sum(w_ij,2)-2*y_lab.'*(w_ij*z);
+                sum2z = sum(w_bar_ij*(z.^2))+(z.^2).'*sum(w_bar_ij,2)-2*z.'*(w_bar_ij*z);
+                
+                for j= 1:length(z)
+                gz = transpose(2*(z(j)-y_lab).'*w_ij+2*(z(j)-y_un).'*w_bar_ij);
+    
+                end 
+
+                fz=sum1z + 0.5*sum2z;
+              
+        end
+
+        y_un=z;
+        g = gz;
+        fx = fz;
+        
+        
+        if (verbosity>0)
+            disp(['-----------------** ' num2str(it) ' **------------------']);
+            disp(['gnr      = ' num2str(abs(gnr))]);
+            disp(['f(x)     = ' num2str(fx)]);
+            disp(['alpha     = ' num2str(alpha)]);                    
+        end
+                     
+        it = it+1;
+        
+        
+end
+
+if(it<maxit)
+    fh(it+1:maxit)=fh(it);
+    gnrit(it+1:maxit)=gnrit(it);
+    timeVec(it+1:maxit)=timeVec(it);
+end
+
+
+ttot = toc;
+
+
+end
--- LabelHW1.m
+++ LabelHW1.m
@@ -1,3 +1,4 @@
+%% Genero i punti e li etichetto in base alla classe
 %Codice utile per implementare un risultato simile a quello che voglio
 %https://it.mathworks.com/matlabcentral/answers/461354-label-my-data-set-automatically-and-group-the-similar-points-or-the-nearest-points-with-the-same
 
@@ -50,5 +51,133 @@ w_ij = pdist2(X_lab,X_un);
 %distanza tra i vari unlabeled 
 w_bar_ij = pdist2(X_un,X_un);
 
-%calcolo il gradiente rispetto agli unlabeled componente per componente
+%scrivo un ciclo for in cui itero itero in k ed aggiorno i gradienti
+%la funzione GM_lab calcola il gradiente in funzione del vettore di
+%instanze unlabeled
+%grad = GM_Lab(y_un,y_lab,w_ij,w_bar_ij);
+
+%calcolo il gradiente come prodotto tra matrici
+
+ for j= 1:length(y_un)
+    grad = transpose(2*(y_un(j)-y_lab).'*w_ij+2*(y_un(j)-y_un).'*w_bar_ij);
+    
+ end 
+
+
+%norma del gradiente
+gnr = grad.'*grad;
+
+
+%% Discesa del gradiente con alpha fissato
+%Costante di Lipschitz e numero massimo di iterazioni
+lc = 12;
+alpha = 1/lc;
+maxniter = 10000;
+
+%contatore di iterazioni
+it = 1;
+
+%valore della funzione
+sum1 = sum(w_ij*(y_un.^2))+(y_lab.^2).'*sum(w_ij,2)-2*y_lab.'*(w_ij*y_un);
+sum2 = sum(w_bar_ij*(y_un.^2))+(y_un.^2).'*sum(w_bar_ij,2)-2*y_un.'*(w_bar_ij*y_un);
+
+fx=sum1 + 0.5*sum2;   %nella notazione del prof è fx nel nostro caso sarebbe fy
+
+stopcr = 2; %l'algoritmo si ferma quando il gradiente è vicino al minimo
+verbosity =1; %scive gli update del gradiente se >0
+
+while (1)
+    
+    if (it>=maxniter)
+        break;
+    end
+    switch stopcr
+            case 1
+                % continue if not yet reached target value fstop
+                if (fx<=fstop)
+                    break
+                end
+            case 2
+                % stopping criterion based on the product of the 
+                % gradient with the direction
+                if (abs(gnr) <= eps)
+                    break;
+                end
+            otherwise
+                error('Unknown stopping criterion');
+    end % end of the stopping criteria switch
+    
+    d=-grad;
+    gnr = grad'*d;
+    gnrit(it) = -gnr;
+    
+            z=y_un+alpha*d;
+            sum1z = sum(w_ij*(z.^2))+(y_lab.^2).'*sum(w_ij,2)-2*y_lab.'*(w_ij*z);
+            sum2z = sum(w_bar_ij*(z.^2))+(z.^2).'*sum(w_bar_ij,2)-2*z.'*(w_bar_ij*z);
+            fz=sum1z + 0.5*sum2z;
+            
+                
+            for j= 1:length(z)
+            gz = transpose(2*(z(j)-y_lab).'*w_ij+2*(z(j)-y_un).'*w_bar_ij);
+            end 
+           
+            
+            if (verbosity>0)
+            disp(['-----------------** ' num2str(it) ' **------------------']);
+            disp(['gnr      = ' num2str(abs(gnr))]);
+            disp(['f(x)     = ' num2str(fx)]);
+            disp(['alpha     = ' num2str(alpha)]);                    
+            end
+        grad = gz; 
+        y_un= z;
+        it = it+1;
+    
+    
+    
+end
+
+%% Inserisco i parametri per provare la funzione G_descent
+
+% Optimality tolerance:
+eps = 1.0e-4;
+% Stopping criterion
+%
+% 1 : reach of a target value for the obj.func. fk - fstop <= eps
+% 2 : nabla f(xk)'dk <= eps
+stopcr = 2;
+
+%verbosity =0 doesn't display info, verbosity =1 display info
+verb=1;
+
+%Valore della Lipschitz constant dato a caso, bisogna calcolarlo come
+%massimo degli autovettori
+lc = 100;
+
+% starting point, sono gli unlabled
+x1= y_un;
+
+fstop = 0;
+maxit = 10000;
+arls=3;
+
+disp('*****************');
+disp('*  GM STANDARD  *');
+disp('*****************');
+
+%ygm è il vettore delle previsioni prodotte dal metodo (cioè il minimo
+%della funzione a cui sono interesato
+%itergm è il numero di iterazioni fatte dal metodo
+[ygm,itergm,fxgm,tottimegm,fhgm,timeVecgm,gnrgm]=...
+G_descent(w_ij,y_lab,w_bar_ij,y_un,lc,verb,arls,maxit,eps,fstop,stopcr);
+
+
+
+% Print results:%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf(1,'0.5*xQX - cx  = %10.3e\n',fxgm);
+%fprintf(1,'Number of non-zero components of x = %d\n',...
+%   sum((abs(xgm)>=0.000001)));
+fprintf(1,'Number of iterations = %d\n',itergm);
+fprintf(1,'||gr||^2 = %d\n',gnrgm(maxit));
+fprintf(1,'CPU time so far = %10.3e\n', tottimegm);
+
 
